505721567adec04795f2b5e419f1d71767f78dd0
Maze Peterson <mazecodes@protonmail.com>
2021-01-26 21:31:45 -0800

    Add JSDoc for the constructor

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 class Emitly {
+  /**
+   * @constructor
+   *
+   * @param {Object} [options] - Options object (optional)
+   * @param {Boolean} [options.caseSensitive] - If events should be case sensitive (optional)
+   * @returns {void}
+   *
+   * @example
+   *   new Emitly()
+   *
+   *   new Emitly({
+   *     caseSensitive: false
+   *   })
+   */
   constructor(options = {}) {
     this.caseSensitive =
       typeof options.caseSensitive !== 'undefined'
